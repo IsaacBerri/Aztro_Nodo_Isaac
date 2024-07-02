75f9d02015a9cf527c3b5d4c33c180eaa0574daa
Isaac Berrio <berriisaax18@hotmail.com>
2024-07-02 14:42:39 -0500

    Reto 5 casi listo (Falta el css)

---
+++ Reto 5- LOGICA SECUENCIAL 1 y 2/scripts/main.js
@@ -0,0 +1,26 @@
+// const inputs = document.querySelectorAll(".sectionInputs__input");
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (e.target.name === "rectangulo") {
+    calcularRectangulo(e);
+  } else {
+    calcularCirculo(e);
+  }
+});
+
+const calcularRectangulo = (e) => {
+  const base = Number(e.target["base"].value);
+  const altura = Number(e.target["altura"].value);
+  e.target["area"].value = base * altura;
+  e.target["perimetro"].value = (base + altura) * 2;
+  e.target["diametro"].value = Math.sqrt(base ** 2 + altura ** 2).toFixed(2);
+};
+
+const calcularCirculo = (e) => {
+  const radio = Number(e.target["radio"].value);
+  e.target["area"].value = (Math.PI * radio ** 2).toFixed(2);
+  e.target["perimetro"].value = (radio * 2 * Math.PI).toFixed(2);
+};
